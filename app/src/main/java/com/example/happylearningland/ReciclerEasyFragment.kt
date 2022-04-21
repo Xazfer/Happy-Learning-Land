8a1d4a4d6cd4e69c82dad3d35186a969b522c4e1
MaanicW <wariotanks28@gmail.com>
2022-04-20 21:43:51 -0500

    ReciclerEasy Timer

--- app/src/main/java/com/example/happylearningland/ReciclerEasyFragment.kt
+++ app/src/main/java/com/example/happylearningland/ReciclerEasyFragment.kt
@@ -1,24 +1,42 @@
 package com.example.happylearningland
 
+import android.os.Binder
 import android.os.Bundle
-import android.util.Log
+import android.os.CountDownTimer
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
+import android.widget.TextView
 import com.example.happylearningland.databinding.FragmentReciclerEasyBinding
-import com.example.happylearningland.databinding.FragmentReciclerTutorialBinding
 
 
 class ReciclerEasyFragment : Fragment(R.layout.fragment_recicler_easy) {
+
     private lateinit var binding: FragmentReciclerEasyBinding
+    private var txtTiempo: EditText? = null
+    private var tvCuentaAtras: TextView? = null
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding = FragmentReciclerEasyBinding.bind(view)
-        var tiempo = binding.tiempo.text
-        Log.e("Revision", tiempo.toString())
+        txtTiempo = binding.txtTiempo.findViewById(R.id.txtTiempo)
+        tvCuentaAtras = binding.tvCuentaAtras.findViewById(R.id.tvCuentaAtras)
+
     }
-
-
+        fun play(view: View) {
+            var tiempoSegundos = txtTiempo?.text.toString().toLong()
+            var tiempoMilisegundos = tiempoSegundos * 1000
+            object : CountDownTimer(tiempoMilisegundos, 1000) {
+                override fun onFinish() {
+                }
+                override fun onTick(millisUntilFinished: Long) {
+                    val tiempoSegundos = (millisUntilFinished / 1000).toInt() + 1
+                    tvCuentaAtras?.text = tiempoSegundos.toString().padStart(2, '0')
+                }
+            }.start()
+        }
 }
+
+
