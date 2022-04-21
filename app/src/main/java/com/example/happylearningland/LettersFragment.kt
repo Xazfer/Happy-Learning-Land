2b7c2f1633503ae46f7b9f0c7e0d019ea42674c5
yogi-boomer <lufevacecbtis13@gmail.com>
2022-04-20 20:47:18 -0500

    GameLetters and changes to GameRecicler

--- app/src/main/java/com/example/happylearningland/LettersFragment.kt
+++ app/src/main/java/com/example/happylearningland/LettersFragment.kt
@@ -9,7 +9,7 @@ import android.content.ClipDescription
 import android.graphics.Canvas
 import android.graphics.Color
 import android.graphics.Point
-import android.os.Build
+import android.util.Log
 import android.view.DragEvent
 import android.widget.ImageView
 import androidx.core.view.isVisible
@@ -18,20 +18,328 @@ import kotlinx.android.synthetic.main.fragment_letters.*
 
 class LettersFragment : Fragment(R.layout.fragment_letters) {
     private lateinit var binding: FragmentLettersBinding
-    private var nombre:String? = null
+     var num = 1
+     var score = 0
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         //bindeo de elementos del fragment
         binding = FragmentLettersBinding.bind(view)
         //recuperacion de argumentos
+        Log.e("Revision", num.toString())
 
         drag10ImageView.setOnLongClickListener(longClickListener)
         drag11ImageView.setOnLongClickListener(longClickListener)
         drag12ImageView.setOnLongClickListener(longClickListener)
+        drag13ImageView.setOnLongClickListener(longClickListener)
+
+        drag14ImageView.setOnLongClickListener(longClickListener)
+        drag15ImageView.setOnLongClickListener(longClickListener)
+        drag16ImageView.setOnLongClickListener(longClickListener)
+        drag17ImageView.setOnLongClickListener(longClickListener)
+        drag18ImageView.setOnLongClickListener(longClickListener)
+        drag19ImageView.setOnLongClickListener(longClickListener)
+        drag20ImageView.setOnLongClickListener(longClickListener)
 
         target10ImageView.setOnDragListener(dragListener)
         target11ImageView.setOnDragListener(dragListener2)
         target12ImageView.setOnDragListener(dragListener3)
+
+        setLetters()
+    }
+    fun setLetters(){
+        when(num){
+            1->{
+                imageView.setImageResource(R.drawable.ola)
+                drag10ImageView.setImageResource(R.drawable.abecedarioo)
+                drag11ImageView.setImageResource(R.drawable.abecedariol)
+                drag12ImageView.setImageResource(R.drawable.abecedarioa)
+                drag13ImageView.setImageResource(R.drawable.abecedariop)
+                drag14ImageView.setImageResource(R.drawable.abecedarios)
+                drag15ImageView.setImageResource(R.drawable.abecedariob)
+                drag16ImageView.setImageResource(R.drawable.abecedariou)
+                drag17ImageView.setImageResource(R.drawable.abecedariol)
+                drag18ImageView.setImageResource(R.drawable.abecedarioy)
+                drag19ImageView.setImageResource(R.drawable.abecedariov)
+                drag20ImageView.setImageResource(R.drawable.abecedariop)
+            }
+            2->{
+                imageView.setImageResource(R.drawable.oso)
+                drag10ImageView.setImageResource(R.drawable.abecedarioo)
+                drag11ImageView.setImageResource(R.drawable.abecedarios)
+                drag12ImageView.setImageResource(R.drawable.abecedarioo)
+                drag13ImageView.setImageResource(R.drawable.abecedariop)
+                drag14ImageView.setImageResource(R.drawable.abecedarioz)
+                drag15ImageView.setImageResource(R.drawable.abecedarior)
+                drag16ImageView.setImageResource(R.drawable.abecedarioe)
+                drag17ImageView.setImageResource(R.drawable.abecedariou)
+                drag18ImageView.setImageResource(R.drawable.abecedarioy)
+                drag19ImageView.setImageResource(R.drawable.abecedariov)
+                drag20ImageView.setImageResource(R.drawable.abecedariop)
+            }
+            3->{
+                imageView.setImageResource(R.drawable.rey)
+                drag10ImageView.setImageResource(R.drawable.abecedarior)
+                drag11ImageView.setImageResource(R.drawable.abecedarioe)
+                drag12ImageView.setImageResource(R.drawable.abecedarioy)
+                drag13ImageView.setImageResource(R.drawable.abecedarioa)
+                drag14ImageView.setImageResource(R.drawable.abecedarioz)
+                drag15ImageView.setImageResource(R.drawable.abecedariob)
+                drag16ImageView.setImageResource(R.drawable.abecedariou)
+                drag17ImageView.setImageResource(R.drawable.abecedariol)
+                drag18ImageView.setImageResource(R.drawable.abecedarioy)
+                drag19ImageView.setImageResource(R.drawable.abecedariov)
+                drag20ImageView.setImageResource(R.drawable.abecedariop)
+            }
+            4->{
+                imageView.setImageResource(R.drawable.pez)
+                drag10ImageView.setImageResource(R.drawable.abecedariop)
+                drag11ImageView.setImageResource(R.drawable.abecedarioe)
+                drag12ImageView.setImageResource(R.drawable.abecedarioz)
+                drag13ImageView.setImageResource(R.drawable.abecedarioa)
+                drag14ImageView.setImageResource(R.drawable.abecedarios)
+                drag15ImageView.setImageResource(R.drawable.abecedarior)
+                drag16ImageView.setImageResource(R.drawable.abecedarioi)
+                drag17ImageView.setImageResource(R.drawable.abecedariou)
+                drag18ImageView.setImageResource(R.drawable.abecedarioy)
+                drag19ImageView.setImageResource(R.drawable.abecedarios)
+                drag20ImageView.setImageResource(R.drawable.abecedarioa)
+            }
+            5->{
+                imageView.setImageResource(R.drawable.uva)
+                drag10ImageView.setImageResource(R.drawable.abecedariou)
+                drag11ImageView.setImageResource(R.drawable.abecedariov)
+                drag12ImageView.setImageResource(R.drawable.abecedarioa)
+                drag13ImageView.setImageResource(R.drawable.abecedarior)
+                drag14ImageView.setImageResource(R.drawable.abecedarioi)
+                drag15ImageView.setImageResource(R.drawable.abecedariob)
+                drag16ImageView.setImageResource(R.drawable.abecedariou)
+                drag17ImageView.setImageResource(R.drawable.abecedariol)
+                drag18ImageView.setImageResource(R.drawable.abecedarioy)
+                drag19ImageView.setImageResource(R.drawable.abecedariob)
+                drag20ImageView.setImageResource(R.drawable.abecedariop)
+            }
+            6->{
+                imageView.setImageResource(R.drawable.rio)
+                drag10ImageView.setImageResource(R.drawable.abecedarior)
+                drag11ImageView.setImageResource(R.drawable.abecedarioi)
+                drag12ImageView.setImageResource(R.drawable.abecedarioo)
+                drag13ImageView.setImageResource(R.drawable.abecedarioa)
+                drag14ImageView.setImageResource(R.drawable.abecedarioz)
+                drag15ImageView.setImageResource(R.drawable.abecedariob)
+                drag16ImageView.setImageResource(R.drawable.abecedariou)
+                drag17ImageView.setImageResource(R.drawable.abecedariol)
+                drag18ImageView.setImageResource(R.drawable.abecedarioy)
+                drag19ImageView.setImageResource(R.drawable.abecedariov)
+                drag20ImageView.setImageResource(R.drawable.abecedariop)
+            }
+            7->{
+                imageView.setImageResource(R.drawable.ave)
+                drag10ImageView.setImageResource(R.drawable.abecedarioa)
+                drag11ImageView.setImageResource(R.drawable.abecedariov)
+                drag12ImageView.setImageResource(R.drawable.abecedarioe)
+                drag13ImageView.setImageResource(R.drawable.abecedarioa)
+                drag14ImageView.setImageResource(R.drawable.abecedarioz)
+                drag15ImageView.setImageResource(R.drawable.abecedariop)
+                drag16ImageView.setImageResource(R.drawable.abecedariob)
+                drag17ImageView.setImageResource(R.drawable.abecedariou)
+                drag18ImageView.setImageResource(R.drawable.abecedarioy)
+                drag19ImageView.setImageResource(R.drawable.abecedariov)
+                drag20ImageView.setImageResource(R.drawable.abecedariop)
+            }
+            8->{
+                imageView.setImageResource(R.drawable.sol)
+                drag10ImageView.setImageResource(R.drawable.abecedarios)
+                drag11ImageView.setImageResource(R.drawable.abecedarioo)
+                drag12ImageView.setImageResource(R.drawable.abecedariol)
+                drag13ImageView.setImageResource(R.drawable.abecedarior)
+                drag14ImageView.setImageResource(R.drawable.abecedariou)
+                drag15ImageView.setImageResource(R.drawable.abecedariob)
+                drag16ImageView.setImageResource(R.drawable.abecedariop)
+                drag17ImageView.setImageResource(R.drawable.abecedarioi)
+                drag18ImageView.setImageResource(R.drawable.abecedarioy)
+                drag19ImageView.setImageResource(R.drawable.abecedariov)
+                drag20ImageView.setImageResource(R.drawable.abecedariop)
+            }
+        }
+    }
+    fun wordCaseOne(){
+        when(num){
+            1->{
+                target10ImageView.setImageResource(R.drawable.abecedarioo)
+                drag10ImageView.isVisible = false
+            }
+            2->{
+                target10ImageView.setImageResource(R.drawable.abecedarioo)
+                drag10ImageView.isVisible = false
+            }
+            3->{
+                target10ImageView.setImageResource(R.drawable.abecedarior)
+                drag10ImageView.isVisible = false
+            }
+            4->{
+                target10ImageView.setImageResource(R.drawable.abecedariop)
+                drag10ImageView.isVisible = false
+            }
+            5->{
+                target10ImageView.setImageResource(R.drawable.abecedariou)
+                drag10ImageView.isVisible = false
+            }
+            6->{
+                target10ImageView.setImageResource(R.drawable.abecedarior)
+                drag10ImageView.isVisible = false
+            }
+            7->{
+                target10ImageView.setImageResource(R.drawable.abecedarioa)
+                drag10ImageView.isVisible = false
+            }
+            8->{
+                target10ImageView.setImageResource(R.drawable.abecedarios)
+                drag10ImageView.isVisible = false
+            }
+        }
+    }
+    fun wordCaseTwo(){
+        when(num){
+            1->{
+                target11ImageView.setImageResource(R.drawable.abecedariol)
+                drag11ImageView.isVisible = false
+            }
+            2->{
+                target11ImageView.setImageResource(R.drawable.abecedarios)
+                drag11ImageView.isVisible = false
+            }
+            3->{
+                target11ImageView.setImageResource(R.drawable.abecedarioe)
+                drag11ImageView.isVisible = false
+            }
+            4->{
+                target11ImageView.setImageResource(R.drawable.abecedarioe)
+                drag11ImageView.isVisible = false
+            }
+            5->{
+                target11ImageView.setImageResource(R.drawable.abecedariov)
+                drag11ImageView.isVisible = false
+            }
+            6->{
+                target11ImageView.setImageResource(R.drawable.abecedarioi)
+                drag11ImageView.isVisible = false
+            }
+            7->{
+                target11ImageView.setImageResource(R.drawable.abecedariov)
+                drag11ImageView.isVisible = false
+            }
+            8->{
+                target11ImageView.setImageResource(R.drawable.abecedarioo)
+                drag11ImageView.isVisible = false
+            }
+        }
+    }
+    fun wordCaseTrhee(){
+        when(num){
+            1->{
+                target12ImageView.setImageResource(R.drawable.abecedarioa)
+                drag12ImageView.isVisible = false
+            }
+            2->{
+                target12ImageView.setImageResource(R.drawable.abecedarioo)
+                drag12ImageView.isVisible = false
+            }
+            3->{
+                target12ImageView.setImageResource(R.drawable.abecedarioy)
+                drag12ImageView.isVisible = false
+            }
+            4->{
+                target12ImageView.setImageResource(R.drawable.abecedarioz)
+                drag12ImageView.isVisible = false
+            }
+            5->{
+                target12ImageView.setImageResource(R.drawable.abecedarioa)
+                drag12ImageView.isVisible = false
+            }
+            6->{
+                target12ImageView.setImageResource(R.drawable.abecedarioo)
+                drag12ImageView.isVisible = false
+            }
+            7->{
+                target12ImageView.setImageResource(R.drawable.abecedarioe)
+                drag12ImageView.isVisible = false
+            }
+            8->{
+                target12ImageView.setImageResource(R.drawable.abecedariol)
+                drag12ImageView.isVisible = false
+            }
+        }
+    }
+    fun revelateLetter(){
+        drag10ImageView.isVisible = true
+        drag11ImageView.isVisible = true
+        drag12ImageView.isVisible = true
+    }
+    fun cleanTarget(){
+        target10ImageView.setImageResource(R.drawable.btn_letters)
+        target11ImageView.setImageResource(R.drawable.btn_letters)
+        target12ImageView.setImageResource(R.drawable.btn_letters)
+    }
+    fun updateScore(){
+        score += 1
+        when(score){
+            3->{
+                num += 1
+                Log.e("Revision", num.toString())
+                revelateLetter()
+                setLetters()
+                cleanTarget()
+            }
+            6->{
+                num += 1
+                Log.e("Revision", num.toString())
+                revelateLetter()
+                setLetters()
+                cleanTarget()
+            }
+            9->{
+                num += 1
+                Log.e("Revision", num.toString())
+                revelateLetter()
+                setLetters()
+                cleanTarget()
+            }
+            12->{
+                num += 1
+                Log.e("Revision", num.toString())
+                revelateLetter()
+                setLetters()
+                cleanTarget()
+            }
+            15->{
+                num += 1
+                Log.e("Revision", num.toString())
+                revelateLetter()
+                setLetters()
+                cleanTarget()
+            }
+            18->{
+                num += 1
+                Log.e("Revision", num.toString())
+                revelateLetter()
+                setLetters()
+                cleanTarget()
+            }
+            21->{
+                num += 1
+                Log.e("Revision", num.toString())
+                revelateLetter()
+                setLetters()
+                cleanTarget()
+            }
+            24->{
+                num += 1
+                Log.e("Revision", num.toString())
+                revelateLetter()
+                setLetters()
+                cleanTarget()
+            }
+        }
     }
     private class MyDragShadowBuilder(val v: View) : View.DragShadowBuilder(v) {
 
@@ -52,15 +360,8 @@ class LettersFragment : Fragment(R.layout.fragment_letters) {
             arrayOf(ClipDescription.MIMETYPE_TEXT_PLAIN),
             item
         )
-
         val myShadow = MyDragShadowBuilder(v)
-
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
-            v.startDragAndDrop(dragData, myShadow,null,0)
-        } else {
-            v.startDrag(dragData, myShadow,null,0)
-        }
-
+        v.startDragAndDrop(dragData, myShadow,null,0)
         true
     }
 
@@ -90,12 +391,16 @@ class LettersFragment : Fragment(R.layout.fragment_letters) {
 
             DragEvent.ACTION_DROP -> {
                 if (event.clipDescription.label == receiverView.tag as String) {
-                    target10ImageView.setImageResource(R.drawable.abecedariou)
-                    drag10ImageView.isVisible = false
+                    wordCaseOne()
+                    updateScore()
+                    Log.e("SCORE", score.toString())
                 } else {
-                    receiverView.setColorFilter(Color.RED)
+                    if (event.clipDescription.label == receiverView.tag as String) {
+                        wordCaseOne()
+                        updateScore()
+                        Log.e("SCORE", score.toString())
+                    }
                 }
-                v.invalidate()
                 true
             }
 
@@ -134,12 +439,16 @@ class LettersFragment : Fragment(R.layout.fragment_letters) {
 
             DragEvent.ACTION_DROP -> {
                 if (event.clipDescription.label == receiverView.tag as String) {
-                    target11ImageView.setImageResource(R.drawable.abecedariov)
-                    drag11ImageView.isVisible = false
+                    wordCaseTwo()
+                    updateScore()
+                    Log.e("SCORE", score.toString())
                 } else {
-                    receiverView.setColorFilter(Color.RED)
+                    if (event.clipDescription.label == receiverView.tag as String) {
+                        wordCaseTwo()
+                        updateScore()
+                        Log.e("SCORE", score.toString())
+                    }
                 }
-                v.invalidate()
                 true
             }
 
@@ -178,12 +487,16 @@ class LettersFragment : Fragment(R.layout.fragment_letters) {
 
             DragEvent.ACTION_DROP -> {
                 if (event.clipDescription.label == receiverView.tag as String) {
-                    target12ImageView.setImageResource(R.drawable.abecedarioa)
-                    drag12ImageView.isVisible = false
+                    wordCaseTrhee()
+                    updateScore()
+                    Log.e("SCORE", score.toString())
                 } else {
-                    receiverView.setColorFilter(Color.RED)
+                    if (event.clipDescription.label == receiverView.tag as String) {
+                    wordCaseTrhee()
+                    updateScore()
+                    Log.e("SCORE", score.toString())
+                    }
                 }
-                v.invalidate()
                 true
             }
 
--- app/src/main/java/com/example/happylearningland/ReciclerHardFragment.kt
+++ app/src/main/java/com/example/happylearningland/ReciclerHardFragment.kt
@@ -1,59 +1,22 @@
 package com.example.happylearningland
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.happylearningland.databinding.FragmentReciclerEasyBinding
+import com.example.happylearningland.databinding.FragmentReciclerHardBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+class ReciclerHardFragment : Fragment(R.layout.fragment_recicler_hard) {
+    private lateinit var binding: FragmentReciclerHardBinding
 
-/**
- * A simple [Fragment] subclass.
- * Use the [ReciclerHardFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
-class ReciclerHardFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding = FragmentReciclerHardBinding.bind(view)
+        var tiempo = binding.tiempo.text
+        Log.e("Revision", tiempo.toString())
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_recicler_hard, container, false)
-    }
-
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment ReciclerHardFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            ReciclerHardFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
-    }
 }
